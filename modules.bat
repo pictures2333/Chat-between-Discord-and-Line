96a531118df18e57b8716b53ef2c28e318aa514e
pour33142GX <85096042+pictures2333@users.noreply.github.com>
2022-07-18 20:56:50 +0800

    modules installation

---
+++ modules.bat
@@ -0,0 +1,8 @@
+pip install -U git+https://github.com/Rapptz/discord.py
+pip install py-cord
+pip install aiohttp
+pip install asyncio
+pip install requests
+pip install flask
+pip install line-bot-sdk
+pip install imgurpython
